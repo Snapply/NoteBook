e2855bcd5ae8d8aa3612edf8813cc86fb5feee48
Snapply <350624690@qq.com>
2016-08-17 17:21:53 +0800

    优化

--- app/src/main/java/com/example/snapply/notebook/Main_Page.java
+++ app/src/main/java/com/example/snapply/notebook/Main_Page.java
@@ -1,9 +1,7 @@
 package com.example.snapply.notebook;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -15,41 +13,11 @@ import android.widget.Button;
  */
 public class Main_Page extends Activity {
 
-    //private String title;
-
-    //private String content;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main_page);
-        /*
-        Intent intent = getIntent();
-        title = intent.getStringExtra("add_title");
-        content = intent.getStringExtra("add_content");
-        MySQLiteOpenHelper dbHelper = new MySQLiteOpenHelper(this,"Data",null,1);
-        //dbHelper.getWritableDatabase();
-        if (!title.isEmpty() && !content.isEmpty()) {
-            SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-            //使用事物
-            db.beginTransaction();
-            try {
-                ContentValues values = new ContentValues();
-                values.put("title",title);
-                values.put("content",content);
-                db.insert("Data",null,values);
-                db.setTransactionSuccessful();
-                values.clear();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                db.endTransaction();
-            }
-            db.close();
-        }
-        */
         Button add = (Button)findViewById(R.id.add_button);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
